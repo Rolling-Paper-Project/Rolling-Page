be97bb6b965d384f73245ea8224ab81c06c4ced7
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-08-02 17:22:05 +0900

    :rotating_light: modify : react DOM 코드 수정
    react 18ver에 맞는 코드로 index.tsx 내 DOM 코드를 수정

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./app/app";
 var rootElement = document.getElementById("root");
 if (!rootElement)
     throw new Error("Failed to find the root element");
-var root = ReactDOM.createRoot(rootElement);
+var root = createRoot(rootElement);
 root.render(React.createElement(React.StrictMode, null,
     React.createElement(App, null)));
 //# sourceMappingURL=index.js.map
