7793704c1bd0677fe094b79eb6a4494b9f73ed3a
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-08-02 18:41:25 +0900

    :lipstick: add : global style, reset css 추가
    reset css와 함께 전역에서 쓰일 ir, hide 클래스 추가

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,37 @@
 import * as React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset"; // style-reset 패키지
+
+const GlobalStyles = createGlobalStyle` 
+    ${reset}
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    &.hide {
+      display: none;
+    }  
+    &.ir {
+    position: absolute;
+    clip: rect(0,0,0,0);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow:hidden;
+  }
+`;
 
 // FC : function component 라는 의미입니다.
 const App: React.FC = () => {
-  return <h1>Hello TypeScript starter!!!</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello TypeScript starter!!!</h1>
+    </>
+  );
 };
 
 export default App;
