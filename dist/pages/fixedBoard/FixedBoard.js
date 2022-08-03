30d9a076f70fb127201a6d5b07474e1cfcf1ce90
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-08-04 02:50:15 +0900

    :seedling: add : page 파일 및 Main 가코드 추가

---
+++ dist/pages/fixedBoard/FixedBoard.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+var FixedBoard = function () {
+    return (React.createElement(React.Fragment, null,
+        React.createElement("div", null)));
+};
+export default FixedBoard;
+//# sourceMappingURL=FixedBoard.js.map
---
+++ src/pages/fixedBoard/FixedBoard.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Container from "../../components/container/Container";
+
+const FixedBoard = () => {
+  return (
+    <>
+      {/* <Container /> */}
+      <div />
+    </>
+  );
+};
+
+export default FixedBoard;
