18b1b4385e3353ddbf2a416f884a28a1b1442f86
joodb <joodb0126@gmail.com>
2022-08-04 10:43:57 +0900

    :lipstick: 타이틀 input 컴포넌트에 button element 추가 구현
    타이틀 input에 button element추가하여 구현했습니다.

---
+++ src/components/titleInput/titleInputStyle.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { BasicBtn } from "../../elements/button";
+
+const Title = styled.h2`
+  color: #b4b4b4;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 18px;
+  margin-bottom: 5px;
+`;
+
+const TitleWrap = styled.section`
+  width: 1400px;
+  margin: 0 auto 20px;
+`;
+
+const DivFlex = styled.div`
+  display: flex;
+  gap: 25px;
+`;
+
+const TitleBtn = styled(BasicBtn)`
+  flex-shrink: 0;
+`;
+
+export { Title, TitleWrap, DivFlex, TitleBtn };
