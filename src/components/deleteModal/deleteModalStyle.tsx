e97e9349ec6a4a2d6f7c15198b218c202069946d
joodb <joodb0126@gmail.com>
2022-08-04 16:28:07 +0900

    :lipstick: feat : 삭제모달창 UI 구현
    삭제모달창 UI 구현했습니다.

---
+++ src/components/deleteModal/deleteModalStyle.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const ModalBg = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: fixed;
+`;
+
+const ModalWrap = styled.section`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 436px;
+  background: #fff;
+  border-radius: 10px;
+  text-align: center;
+  padding: 40px 0;
+  margin: auto;
+  z-index: 20;
+`;
+
+const ModalTitle = styled.h3`
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 27px;
+  margin-bottom: 3px;
+`;
+
+const ModalExp = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  color: #AFAFAF;
+  margin-bottom: 25px;
+`;
+
+const BtnWrap = styled.div`
+  display: flex;
+  padding: 0 88px;
+  justify-content: space-between;
+`;
+
+export { ModalBg, ModalWrap, ModalTitle, ModalExp, BtnWrap };
