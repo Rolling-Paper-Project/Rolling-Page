8cb609550059311a834d7246af73b6c474406943
sweeeeetpotato <sweet_potato13@naver.com>
2022-08-04 15:52:01 +0900

    :lipstick: style : 스티커&포스트작성 모달 컴포넌트 UI 구현

---
+++ src/components/emojiModal/emojiModalStyle.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components"
+
+export const ModalOver = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 100;
+`
+export const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  min-width: 410px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 30px;
+  background-color: white;
+  border-radius: 10px;
+  box-sizing: border-box;
+`
+
+export const ContentsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+export const EmojiWrapper = styled.div`
+  flex: 1;
+  padding: 20px;
+`
+
+export const EmojiflexBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+  text-align: center;
+`
+
+export const PostWrapper = styled(EmojiWrapper)`
+`
+
+export const TitleText = styled.p`
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 27px;
+  text-align: center;
+  margin-bottom: 40px;
+`
+
+export const Img = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  cursor: pointer;
+`
