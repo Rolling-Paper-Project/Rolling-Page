63634ba9c8b7ee262976c492077b7452f27bf50c
ujin <dklvmf0106@gmail.com>
2022-08-05 18:20:43 +0900

    :adhesive_bandage: modify : post split 텍스트 수정
    git push origin feat/post

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -122,10 +122,10 @@ const Container = ({ done }: TitleProps) => {
       <BoardPostUl>
         {postData?.map(element => {
           const randomIdx = Math.floor(Math.random() * 3 + 1);
-          const randomColor = colorArray[randomIdx].split("☇⚁♘");
+          const randomColor = colorArray[randomIdx].split(",");
           const bgColor = randomColor[0];
           const shadowColor = randomColor[1];
-          const comment = element.content.split(",");
+          const comment = element.content.split("☇⚁♘");
           const content = comment[0];
           const name = comment[1];
           const profile = comment[2];
