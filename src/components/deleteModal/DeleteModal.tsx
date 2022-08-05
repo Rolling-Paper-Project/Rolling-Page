ae0b0e91f088432d6055eaf3379c9227d8aa1661
sweeeeetpotato <sweet_potato13@naver.com>
2022-08-05 11:32:38 +0900

    :sparkles: feat : 포스트 삭제 기능 구현
    포스트의 x버튼 클릭시 삭제 여부 모달창이 뜨고, 취소 버튼 클릭시 모달창 사라지고, 삭제 버튼 클릭시 포스트가 삭제되도록 구현

--- src/components/deleteModal/DeleteModal.tsx
+++ src/components/deleteModal/DeleteModal.tsx
@@ -1,9 +1,35 @@
 import * as React from "react";
+import { useParams } from "react-router";
+import axios from "axios";
 import { BasicBtn, CancelBtn } from "../../elements/buttons/button";
 import './deleteModalStyle'
 import { BtnWrap, ModalBg, ModalExp, ModalTitle, ModalWrap } from "./deleteModalStyle";
+import { BASE_URL, TOKEN } from "../../constants/index";
 
-const DeletModal = () => {
+interface Props {
+  key?: string;
+  closeDeleteModal: () => void;
+}
+
+const DeletModal = ({key, closeDeleteModal} : Props) => {
+  const { id } = useParams();
+  
+  const deletePost = async () => {
+    const url = `${BASE_URL}/post/${id}/comments/${key}`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.delete(url, config);
+      return res;
+    } catch (err) {
+      return err;
+    }
+  };
+  
   return (
     <>
     <ModalWrap>
@@ -11,14 +37,11 @@ const DeletModal = () => {
       <ModalExp>삭제하면 되돌릴 수 없어요! :(</ModalExp>
       <BtnWrap>
         <CancelBtn
-          onClick={() => {
-            ("");
-          }}
-        />
+          onClick={closeDeleteModal}
+        >취소
+        </CancelBtn>
         <BasicBtn
-          onClick={() => {
-              ("");
-            }}
+          onClick={deletePost}
           >삭제
         </BasicBtn>
       </BtnWrap>
