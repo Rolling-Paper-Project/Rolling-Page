7735df9e0f7f71eca862a5dcb462471cc8719d31
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-08-07 02:18:01 +0900

    :truck: Button 경로수정

--- src/components/titleInput/titleInputStyle.tsx
+++ src/components/titleInput/titleInputStyle.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Basic } from "../../elements/buttons/button";
+import { Basic } from "../../hooks/buttons/button";
 
 const Title = styled.h2`
   color: #b4b4b4;
