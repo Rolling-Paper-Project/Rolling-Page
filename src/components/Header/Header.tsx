dae9847e33823eff74ec9b1c740be49a295724de
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-08-08 03:40:26 +0900

    :adhesive_bandage: fix : 로고클릭 시 이동페이지 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { HeaderWrap, HeaderContent, LinkButton, LogoImg } from "./headerStyle";
 import logo from "../../assets/icon-logo.svg";
 import github from "../../assets/icon-github.svg";
@@ -11,8 +12,9 @@ const Header = () => {
     <HeaderWrap>
       <h1 className="ir">롤링페이지</h1>
       <HeaderContent>
-        <LogoImg src={logo} alt="로고" />
-        {/* <img src={logo} alt="로고" /> */}
+        <Link to="/">
+          <LogoImg src={logo} alt="로고" />
+        </Link>
         <LinkButton onClick={onClick}>
           Contact Us
           <img src={github} alt="github" />
