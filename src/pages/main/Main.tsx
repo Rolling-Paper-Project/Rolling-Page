3e906605e0d39a3de7f5e960bc672910a5007256
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-08-22 16:44:59 +0900

    :necktie: update : 메인페이지 변경

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -4,53 +4,15 @@ import { ACCOUNTNAME, TOKEN, BASE_URL } from "../../constants";
 import Header from "../../components/header/Header";
 import { BoardWrap } from "../board/style";
 import { MainContainer, BoardLink, MainExp } from "./style";
+import TitleInput from "../../components/titleInput/TitleInput";
 
 const Main = () => {
-  interface BoardDataProps {
-    id: string;
-    content: string;
-  }
-  interface BoardDataResponse {
-    post: BoardDataProps[];
-  }
-  const [boardData, setBoardData] = React.useState<BoardDataResponse>();
-
-  React.useEffect(() => {
-    const getBoardList = async () => {
-      try {
-        const res: AxiosResponse<any> = await axios.get(
-          `${BASE_URL}/post/${ACCOUNTNAME}/userpost/`,
-          {
-            headers: {
-              Authorization: `Bearer ${TOKEN}`,
-              "Content-type": "application/json",
-            },
-          },
-        );
-        setBoardData(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getBoardList();
-  }, []);
-
   return (
     <>
       <Header />
       <BoardWrap>
-        <MainContainer>
-          <MainExp>이름을 클릭해서 롤링페이지를 작성해주세요 💚</MainExp>
-          {boardData?.post?.map(data => (
-            <BoardLink
-              to={`/board/${data.id}`}
-              key={data.id}
-              state={{ boardTitle: data.content }}
-            >
-              {data.content}
-            </BoardLink>
-          ))}
-        </MainContainer>
+        <TitleInput />
+        <MainContainer>사용법 안내 문구가 들어갑니다</MainContainer>
       </BoardWrap>
     </>
   );
